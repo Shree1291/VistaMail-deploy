f70149f50c23643c6f2425e9f95bacd045cb4ebe
Shriram Birajdar <97882995+Shree1291@users.noreply.github.com>
2024-01-29 00:23:32 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -7,20 +7,17 @@ dotenv.config();
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
-const Connection = () => {
-      
 
-     const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@vistamail.8izscyn.mongodb.net/vistamail?retryWrites=true&w=majority`;
-    try{
-       // mongoose.connect(DB_URI);
-      mongoose.connect(DB_URI, {
-        serverSelectionTimeoutMS: 30000
-      });
-        mongoose.set('strictQuery', false);
-        console.log("Database connected successfully!");
-    } catch(err) {
-        console.log("Error while connecting with the database ", err.message);
-    }
-}
+const Connection = async () => {
+  const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-jybynsp-shard-00-00.8izscyn.mongodb.net:27017,ac-jybynsp-shard-00-01.8izscyn.mongodb.net:27017,ac-jybynsp-shard-00-02.8izscyn.mongodb.net:27017/?ssl=true&replicaSet=atlas-ocynsr-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+  try {
+    await mongoose.connect(DB_URI);
+    mongoose.set('strictQuery', false);
+    console.log("Database connected successfully!");
+  } catch (err) {
+    console.error("Error while connecting with the database:", err.message);
+  }
+};
 
 export default Connection;
