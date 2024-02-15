04516b5780c5dcc9c46c0e1b2e29b9ec481018f1
Shriram Birajdar <97882995+Shree1291@users.noreply.github.com>
2024-02-15 15:21:02 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,26 +1,4 @@
 
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const USERNAME = process.env.DB_USERNAME;
-// const PASSWORD = process.env.DB_PASSWORD;
-
-
-// const Connection = async () => {
-//   const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-jybynsp-shard-00-00.8izscyn.mongodb.net:27017,ac-jybynsp-shard-00-01.8izscyn.mongodb.net:27017,ac-jybynsp-shard-00-02.8izscyn.mongodb.net:27017/?ssl=true&replicaSet=atlas-ocynsr-shard-0&authSource=admin&retryWrites=true&w=majority`;
-
-//   try {
-//     await mongoose.connect(DB_URI);
-//     mongoose.set('strictQuery', false);
-//     console.log("Database connected successfully!");
-//   } catch (err) {
-//     console.error("Error while connecting with the database:", err.message);
-//   }
-// };
-
-// export default Connection;
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
@@ -29,28 +7,17 @@ dotenv.config();
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
-const Connection = () => {
 
-    var MongoClient = require('mongodb').MongoClient;
+const Connection = async () => {
+  const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-jybynsp-shard-00-00.8izscyn.mongodb.net:27017,ac-jybynsp-shard-00-01.8izscyn.mongodb.net:27017,ac-jybynsp-shard-00-02.8izscyn.mongodb.net:27017/?ssl=true&replicaSet=atlas-ocynsr-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
-    var uri = "mongodb://${USERNAME}:${PASSWORD}@ac-zdl2ods-shard-00-00.tms7t5v.mongodb.net:27017,ac-zdl2ods-shard-00-01.tms7t5v.mongodb.net:27017,ac-zdl2ods-shard-00-02.tms7t5v.mongodb.net:27017/?ssl=true&replicaSet=atlas-d547jx-shard-0&authSource=admin&retryWrites=true&w=majority";
-
-    //const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-zdl2ods-shard-00-00.tms7t5v.mongodb.net:27017,ac-zdl2ods-shard-00-01.tms7t5v.mongodb.net:27017,ac-zdl2ods-shard-00-02.tms7t5v.mongodb.net:27017/?ssl=true&replicaSet=atlas-d547jx-shard-0&authSource=admin&retryWrites=true&w=majority`; 
-    try{
-        MongoClient.connect(uri, function(err, client) {
-            const collection = client.db("test").collection("devices");
-            client.close();
-        });
-          
-        // mongoose.connect(DB_URI);
-        mongoose.set('strictQuery', false);
-        console.log("Database connected successfully!");
-    } catch(err) {
-        console.log("Error while connecting with the database ", err.message);
-    }
-}
+  try {
+    await mongoose.connect(DB_URI);
+    mongoose.set('strictQuery', false);
+    console.log("Database connected successfully!");
+  } catch (err) {
+    console.error("Error while connecting with the database:", err.message);
+  }
+};
 
 export default Connection;
-
-
-
