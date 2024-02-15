d32c9507a0dee57bf38fd5c16342215e474f0e30
Shriram Birajdar <97882995+Shree1291@users.noreply.github.com>
2024-02-15 18:56:45 +0530

    Update Main.jsx

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -7,9 +7,9 @@ import { Box, styled } from '@mui/material';
 import SuspenseLoader from '../components/common/SuspenseLoader';
 
 
-const Wrapper = styled(Box)`
-    display: flex;
-`;
+// const Wrapper = styled(Box)`
+//     display: flex;
+// `;
 
 
 const Main = () =>{
@@ -21,14 +21,48 @@ const Main = () =>{
     return (
         <>
         <Header toggleDrawer={toggleDrawer} />
-        <Wrapper>
-            <SideBar toggleDrawer={toggleDrawer} openDrawer={openDrawer} />
+        <Box>
+            <SideBar openDrawer={openDrawer} />
             <Suspense fallback={<SuspenseLoader />} >
                 <Outlet context={{ openDrawer }} />
             </Suspense>
-        </Wrapper>
+        </Box>
     </>
     );
-}
+};
 
 export default Main;
+
+// import { useState ,Suspense} from "react";
+// import Header from "../components/Header";
+// import SideBar from "../components/SideBar";
+// import { Outlet } from 'react-router-dom';
+// import { Box, styled } from '@mui/material';
+// import SuspenseLoader from '../components/common/SuspenseLoader';
+
+
+// const Wrapper = styled(Box)`
+//     display: flex;
+// `;
+
+
+// const Main = () =>{
+//     const [ openDrawer,setOpenDrawer] = useState(true);
+
+//     const toggleDrawer = () => {
+//         setOpenDrawer(prevState => !prevState);
+//     }
+//     return (
+//         <>
+//         <Header toggleDrawer={toggleDrawer} />
+//         <Wrapper>
+//             <SideBar toggleDrawer={toggleDrawer} openDrawer={openDrawer} />
+//             <Suspense fallback={<SuspenseLoader />} >
+//                 <Outlet context={{ openDrawer }} />
+//             </Suspense>
+//         </Wrapper>
+//     </>
+//     );
+// }
+
+// export default Main;
